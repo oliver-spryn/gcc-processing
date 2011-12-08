1ce1a4ea7417cb59144afc7abff0515ba0087cd9
wot200@gmail.com <wot200@gmail.com@b4804306-4b9f-346b-38f3-29112173b7ec>
2011-12-08 17:31:52 +0000

    This revision removes or renames several of the Exception classes, updates the testing file, and alters the connection methods in the Multicaster class.

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,23 @@
 import javax.swing.JApplet;
 
+import processing.core.*;
+
 import edu.gcc.processing.net.*;
 import edu.gcc.processing.develop.*;
 import edu.gcc.processing.exceptions.multicaster.MulticasterInitException;
+import edu.gcc.processing.exceptions.net.IPAddressNumericException;
+import edu.gcc.processing.exceptions.net.IPAddressSyntaxException;
 
-public class Test extends JApplet {
-	public void Test() {
-		Multicaster multicast = new Multicaster();
+public class Test extends PApplet {
+	public void setup() {
+		Multicaster multicast = new Multicaster("239.255.255.255");
 		
 		try {
 			multicast.connect();
 		} catch (MulticasterInitException e) {
-			new Message(e.getMessage());
+			// TODO: handle exception
 		}
+		
+		multicast.groupStatus();
 	}
 }
